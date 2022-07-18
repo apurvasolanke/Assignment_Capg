389330fa6c5031c0329af16a5e072ce042323b73
Raosaheb Solanke <APURVA.RAOSAHEB-SOLANKE@CAPGEMINI.COM>
2022-07-18 11:38:01 +0530

    web development Assignment

--- Spring Rest/SpringRESTQ7/src/main/java/com/Q7/Repository/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Q7.Repository;
-
-import com.Q7.model.Order;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepo extends MongoRepository<Order,String> {
-}
--- Spring Rest/SpringRESTQ9/src/main/java/com/Q9/Repository/CustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Q9.Repository;
-
-
-import com.Q9.model.customer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepo extends CrudRepository<customer,Integer>{}
